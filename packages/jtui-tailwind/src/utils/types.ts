9ed7f25049fa8e77e4579ca09e4004749c94693b
jt <jt1xx9@gmail.com>
2024-06-23 11:29:37 +0700

    start: Start build ui library with tailwind css

---
+++ packages/jtui-tailwind/src/utils/types.ts
@@ -0,0 +1,59 @@
+export type TColor =
+    | "slate"
+    | "gray"
+    | "zinc"
+    | "neutral"
+    | "stone"
+    | "red"
+    | "orange"
+    | "amber"
+    | "yellow"
+    | "lime"
+    | "green"
+    | "emerald"
+    | "teal"
+    | "cyan"
+    | "sky"
+    | "blue"
+    | "indigo"
+    | "violet"
+    | "purple"
+    | "fuchsia"
+    | "pink"
+    | "rose";
+
+export type TFontSize =
+    | "xs"
+    | "sm"
+    | "base"
+    | "lg"
+    | "xl"
+    | "2xl"
+    | "3xl"
+    | "4xl"
+    | "5xl"
+    | "6xl"
+    | "7xl"
+    | "8xl"
+    | "9xl";
+
+export type TBorderRadius =
+    | "none"
+    | "sm"
+    | "base"
+    | "md"
+    | "lg"
+    | "xl"
+    | "2xl"
+    | "3xl"
+    | "full";
+
+export type TShadow =
+    | "sm"
+    | "base"
+    | "md"
+    | "lg"
+    | "xl"
+    | "2xl"
+    | "inner"
+    | "none";
